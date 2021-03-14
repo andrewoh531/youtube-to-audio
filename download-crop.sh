c5062e0e9853654162bbe044cdd0f41a7c426eed
andrewoh531 <andrewoh531@gmail.com>
2021-03-14 15:55:12 +1100

    Fix the script

--- download-crop.sh
+++ download-crop.sh
@@ -18,11 +18,7 @@ DOWNLOADED_FILENAME="${TODAYS_DATE}-original.mp3"
 EDITED_FILENAME="${TODAYS_DATE}-pruned.mp3"
 
 echo "Downloading $1..."
-#youtube-dl -i --no-mark-watched --extract-audio --audio-format mp3 --audio-quality 0 --output ${DOWNLOADED_FILENAME} $1
+youtube-dl -i --no-mark-watched --extract-audio --audio-format mp3 --audio-quality 0 --output ${DOWNLOADED_FILENAME} $1
 
 echo "Editing $1..."
 ffmpeg -ss $2 -to $3 -i "${DOWNLOADED_FILENAME}" -c:a libmp3lame "${EDITED_FILENAME}"
-# ffmpeg -ss 00:32:50.00 -to 01:12:25.00 -i "2021-02-28-original.mp3" -c:a libmp3lame asdfasdf.mp3
-
-
-ffmpeg -ss 32:50 -to 1:12:25 -i "2021-02-28-original.mp3" -c:a libmp3lame test.mp3
